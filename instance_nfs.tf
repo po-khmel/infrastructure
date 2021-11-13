bbdfb16f948263aea635aceca1954640416742b8
pmandreoli <pietro.mandreoli@gmail.com>
2021-11-13 12:13:51 +0100

    add volumes to NFS instance

--- instance_nfs.tf
+++ instance_nfs.tf
@@ -2,65 +2,73 @@ resource "openstack_compute_instance_v2" "nfs-server" {
 
   name            = "condor_nfs"
   image_name      = "${var.vgcn_image}"
-  flavor_name     = "large"
+  flavor_name     = "medium"
   key_pair        = "cloud"
   security_groups = ["public"]
 
   network {
     name = "private_net"
   }
+  user_data = file("files/format_volumes.sh")
+}
+resource "openstack_blockstorage_volume_v2" "tools" {
+  name        = "tools"
+  description = "tools volume"
+  size        = 50
+}
+resource "openstack_blockstorage_volume_v2" "datamanager" {
+  name        = "datamanager"
+  description = "datamanager"
+  size        = 50
+}
+resource "openstack_blockstorage_volume_v2" "sync" {
+  name        = "sync" 
+  description = "sync" 
+  size        = 50
+}
+resource "openstack_blockstorage_volume_v2" "jwd" {
+  name        = "jwd"
+  description = "jwd"
+  size        = 50
+}
+resource "openstack_blockstorage_volume_v2" "dati" {
+  name        = "dati"
+  description = "dati"
+  size        = 70
+}
 
-  block_device {
-    uuid                  = "d92781a6-bbb5-4238-8fd7-c38d2cc8b68f"
-    source_type           = "image"
-    destination_type      = "local"
-    boot_index            = 0
-    delete_on_termination = true
-  }
+resource "openstack_compute_volume_attach_v2" "tools" {
+  instance_id = "${openstack_compute_instance_v2.nfs-server.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.tools.id}"
+  device      = "/dev/vdb"
+  
+}
+resource "openstack_compute_volume_attach_v2" "datamanager" {
+  instance_id = "${openstack_compute_instance_v2.nfs-server.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.datamanager.id}"
+  device      = "/dev/vdc"
+  
+}
 
-  block_device {   
-    uuid                  = "${openstack_blockstorage_volume_v2.volume_nfs_data.id}"
-    source_type           = "volume"
-    destination_type      = "volume"
-    boot_index            = -1
-    delete_on_termination = true
-  }
+resource "openstack_compute_volume_attach_v2" "sync" {
+  instance_id = "${openstack_compute_instance_v2.nfs-server.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.sync.id}"
+  device      = "/dev/vdd"
+  
+}
 
-  user_data = "${data.template_cloudinit_config.nfs-share.rendered}"
+resource "openstack_compute_volume_attach_v2" "jwd" {
+  instance_id = "${openstack_compute_instance_v2.nfs-server.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.jwd.id}"
+  device      = "/dev/vde"
+  
+}
+
+resource "openstack_compute_volume_attach_v2" "dati" {
+  instance_id = "${openstack_compute_instance_v2.nfs-server.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.dati.id}"
+  device      = "/dev/vdf"
+  
 }
 
 
-resource "openstack_blockstorage_volume_v2" "volume_nfs_data" {
-  name = "condor.volume_nfs_data"
-  size = "${var.nfs_disk_size}"
-}
-
-data "template_cloudinit_config" "nfs-share" {
-  gzip          = true
-  base64_encode = true
-
-  part {
-    content_type = "text/x-shellscript"
-    content      = "${file("${path.module}/files/create_share.sh")}"
-  }
-
-  part {
-    content_type = "text/cloud-config"
-    content      = <<-EOF
-    #cloud-config
-    write_files:
-    - content: |
-        /data/share *(rw,sync)
-      owner: root:root
-      path: /etc/exports
-      permissions: '0644'
-
-    runcmd:
-     - [ systemctl, enable, nfs-server ]
-     - [ systemctl, start, nfs-server ]
-     - [ exportfs, -avr ]
-  EOF
-  }
-
-
-}
