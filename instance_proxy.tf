c3e9d80bbd4ab368bebd234adc71971c53df64ce
pmandreoli <pietro.mandreoli@gmail.com>
2022-01-18 18:12:32 +0100

    change image var for new rabbitmq installation

--- instance_proxy.tf
+++ instance_proxy.tf
@@ -1,6 +1,6 @@
 resource "openstack_compute_instance_v2" "rabbitmq" {
   name            = "rabbit.internal.galaxyproject.it"
-  image_name      = "${var.centos_image}"
+  image_name      = "${var.centos8_image}"
   flavor_name     = "small"
   key_pair        = "cloud"
   security_groups = ["egress", "public-ssh", "public-ping", "public-web2", "public-amqp","default"]
