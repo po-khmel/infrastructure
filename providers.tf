1c5913e847c3333abf148f125ca03a6ca1af7c3e
po-khmel <khmelevskayapv@gmail.com>
2023-04-19 20:11:33 +0200

    update versions

--- providers.tf
+++ providers.tf
@@ -1,7 +1,21 @@
-provider "openstack" {
-  version = "1.21"
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "1.48.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.76.1"
+    }
+  }
 }
 
-provider "aws" {
-  version = "2.22"
-}
+# provider "openstack" {
+#   version = "1.21"
+# }
+
+# provider "aws" {
+#   version = "2.22"
+# }
