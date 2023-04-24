aae09622eb8adb2b1891ac38efeb32549e48ea2e
po-khmel <khmelevskayapv@gmail.com>
2023-04-24 13:13:43 +0200

    update images

--- vars.tf
+++ vars.tf
@@ -1,19 +1,29 @@
+# variable "centos_image" {
+#   default = "vggp-v31-j132-4ab83d5ffde9-master" # ftp image
+# }
 
-variable "centos_image" {
-  default = "vggp-v31-j132-4ab83d5ffde9-master"
-}
 variable "centos8_image" {
-  default = "CentOS-8-ec2-8.4.2105-20210603.0.x86_64"
+  type = map
+  default = {
+    "name" = "CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64"
+    "id"   = "b3282e0e-0848-453b-9ff4-f799222ed82f"
+  }
 }
+
 variable "ubuntu20-04_image" {
-  default = "79f390c9-88ce-418e-9325-c372ba29473c"
+  type = map
+  default = {
+    "name" = "Ubuntu Server 20.04 LTS (Focal Fossa)"
+    "id"   = "0f22f335-8175-4e68-b730-44b8d2c1973d"
+  }
 }
+
 variable "vgcn_image"{
-  type = map(string)
+  type = map
   default= { 
-    "name" = "vggp-v40-j214-495f8c33c4b0-master"
-    "id" = "d92781a6-bbb5-4238-8fd7-c38d2cc8b68f"
-}
+    "name" = "vggp-v60-j225-1a1df01ec8f3-dev"
+    "id" = "a2291cc6-4947-4d3c-870e-16e0afccaa75"
+  }
 }
 
 variable "nfs_disk_size" {
