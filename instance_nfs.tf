bec1342db077943b4b876c61c180a272b8c93934
po-khmel <khmelevskayapv@gmail.com>
2023-04-26 13:38:07 +0200

    v3

--- instance_nfs.tf
+++ instance_nfs.tf
@@ -19,7 +19,7 @@ resource "openstack_compute_instance_v2" "nfs-server" {
   }
 
   block_device {
-    uuid                  = "${openstack_blockstorage_volume_v2.nfs_volume.id}"
+    uuid                  = "${openstack_blockstorage_volume_v3.nfs_volume.id}"
     source_type           = "volume"
     destination_type      = "volume"
     boot_index            = -1
