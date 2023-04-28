76af08c21a52db4a124289dc2adc98a36f34a5b0
po-khmel <khmelevskayapv@gmail.com>
2023-04-28 14:34:08 +0200

    lint

--- keypairs.tf
+++ keypairs.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_keypair_v2" "cloud2" {
-  name       = "${var.public_key.name}"
-  public_key = "${var.public_key.public_key}"
+  name       = var.public_key.name
+  public_key = var.public_key.public_key
 }
 
