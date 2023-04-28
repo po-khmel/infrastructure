52d03a1bfe4a5ab2b7acc3539fa3fe2ce1813dad
po-khmel <khmelevskayapv@gmail.com>
2023-04-28 14:34:00 +0200

    add control vm

--- instance_master.tf
+++ instance_master.tf
@@ -1,25 +1,25 @@
-# resource "openstack_compute_instance_v2" "mastervm" {
-#   name            = "usemastervm.controlvm"
-#   image_name      = "${var.controlvm_image.name}"
-#   flavor_name     = "${var.flavors.controlvm}"
-#   key_pair        = "${openstack_compute_keypair_v2.cloud2.name}" 
-#   security_groups = ["egress", "public-ssh","default"]
+resource "openstack_compute_instance_v2" "mastervm" {
+  name            = "usemastervm.controlvm"
+  image_name      = var.controlvm_image.name
+  flavor_name     = var.flavors.controlvm
+  key_pair        = data.openstack_compute_keypair_v2.cloud2.name
+  security_groups = ["egress", "public-ssh", "default"]
 
-#   # network {
-#   #   name = "${var.public_network.name}"
-#   # }
+  # network {
+  #   name = var.public_network.name
+  # }
 
-#   network {
-#     name = "${var.private_network.name}"
-#   }
-# }
+  network {
+    name = var.private_network.name
+  }
+}
 
-# resource "openstack_networking_floatingip_v2" "mastervm_fl_ip" {
-#   pool = "${var.public_network.name}"
-# }
+resource "openstack_networking_floatingip_v2" "mastervm_fl_ip" {
+  pool = var.public_network.name
+}
 
-# resource "openstack_compute_floatingip_associate_v2" "mastervm_fl_ip" {
-#   floating_ip = "${openstack_networking_floatingip_v2.mastervm_fl_ip.address}"
-#   instance_id = "${openstack_compute_instance_v2.mastervm.id}"
-#   fixed_ip    = "${openstack_compute_instance_v2.mastervm.network.0.fixed_ip_v4}"
-# }
+resource "openstack_compute_floatingip_associate_v2" "mastervm_fl_ip" {
+  floating_ip = data.openstack_networking_floatingip_v2.mastervm_fl_ip.address
+  instance_id = data.openstack_compute_instance_v2.mastervm.id
+  fixed_ip    = data.openstack_compute_instance_v2.mastervm.network.0.fixed_ip_v4
+}
